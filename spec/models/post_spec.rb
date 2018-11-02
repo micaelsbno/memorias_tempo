fa3cedbe6f9590991a798b46debc1a6f3c0b1283
micaelsbno <micaelsbno@gmail.com>
2018-11-02 22:28:35 +1100

    add weather specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,23 +8,22 @@ FactoryBot.define do
 end
 
 describe Post do 
-  
   let (:user) { User.create(username: 'Name', password: '123') }
 
   it 'has a valid factory' do
-    post = build(:post, { content: 'Post content', user_id: user.id })
+    post = build(:post, { user_id: user.id })
 
     expect(post.save).to be true
   end
 
   it 'is invalid without any content' do
-    post = build(:post, { content: '', user_id: user.id })
+    post = build(:post, { content: '' })
 
     expect(post.save).to be false
   end
 
   it 'is invalid without an user_id' do
-    post = build(:post, { content: 'Post content', user_id: nil })
+    post = build(:post, { user_id: nil })
 
     expect(post.save).to eq false
   end
--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe Weather, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+FactoryBot.define do
+  factory :weather do
+    post_id { 1 }
+    city { 'Melbourne' }
+    country { 'Australia' }
+    lat { -37.8132 }
+    lon { 144.963 }
+    brief { 'Clear' }
+    description { 'clear sky' }
+  end
+end
+
+describe Weather do
+  let (:user) { User.create(username: 'Name', password: '123') } 
+  let (:post) { Post.create(content: 'Post content', user_id: user.id) }
+
+  it 'has a valid factory' do
+    expect(build(:weather, post_id: post.id).save).to be true
+  end
+
+  it 'is invalid without a city' do 
+    expect(build(:weather, city: '').save).to be false
+  end
+
+  it 'is invalid without a post id' do
+    expect(build(:weather, post_id: nil).save).to be false
+  end
 end
