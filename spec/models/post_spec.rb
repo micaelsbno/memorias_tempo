0d959bbd8436adebf3df3fe5f9919e1efcd6ab37
micaelsbno <micaelsbno@gmail.com>
2018-11-02 16:00:55 +1100

    add post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  context 'new post' do
+FactoryBot.define do
+  factory :post do
+    content { 'Post content' }
+    user_id { 1 }
+  end
+end
+
+describe Post do 
+  
+  let (:user) { User.create(username: 'Name', password: '123') }
+
+  it 'has a valid factory' do
+    post = build(:post, { content: 'Post content', user_id: user.id })
+
+    expect(post.save).to be true
+  end
+
+  it 'is invalid without any content' do
+    post = build(:post, { content: '', user_id: user.id })
+
+    expect(post.save).to be false
+  end
+
+  it 'is invalid without an user_id' do
+    post = build(:post, { content: 'Post content', user_id: nil })
+
+    expect(post.save).to eq false
   end
 end
