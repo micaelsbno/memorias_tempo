45b187e7570a80f3b6840504f3976e8e36797670
micaelsbno <micaelsbno@gmail.com>
2018-11-04 19:52:56 +1100

    change login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'homepage#index'
   get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
 
   resources :users
   resources :posts
-  resource :sessions, only: [:new, :create, :destroy]
+  resource :sessions, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
