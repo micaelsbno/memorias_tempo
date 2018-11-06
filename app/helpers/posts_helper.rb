fb92b164586df811d27985324a00cd8bb038fb99
micaelsbno <micaelsbno@gmail.com>
2018-11-06 20:25:02 +1100

    fix remote ip

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -17,6 +17,7 @@ module PostsHelper
   end
 
   def find_city
+    puts request.remote_ip
     query = HTTParty.get("http://api.ipstack.com/#{request.env['HTTP_X_FORWARDED_FOR']}?access_key=#{ENV['IPSTACK_KEY']}")
     return query['city'] if query['city']
     return query['region_name'] if query['region_name']
