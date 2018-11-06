46f52355573bf4ffe19ead133108338a1b5d8ebb
micaelsbno <micaelsbno@gmail.com>
2018-11-06 20:01:07 +1100

    fix appendPost function

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -9,7 +9,29 @@ postMemory = (e) => {
 appendNewPost = (response) => {
   const posts = document.querySelector('.posts')
   const postDiv = document.createElement('div')
-  postDiv.textContent = response.content
+  postDiv.className = 'post'
+  postDiv.innerHTML = `
+    <p class="post__content">${response.content}</p>
+      <div class="post__weather">
+          <span class="post__weather--icon"><i class="fas fa-sun"></i></span><p class="post__weather--text">${response.weather_description}</p>
+      </div>
+      <div class="post__footer">
+        <p class="post__footer--time">${formatDate(response.created_at)}</p>
+        <p class="post__footer--city">${response.city}</p>
+      </div>
+    </div>
+  `
   posts.insertBefore(postDiv, posts.firstChild)
+}
+
+formatDate = (dateString) => {
+  date = new Date(dateString)
+  string = ``
+  string += Math.abs(date.getHours() - 12)
+  string += `:${date.getMinutes()}`
+  string += `${date.getHours() > 12 ? 'am' : 'pm'}`
+  string += ` – ${date.toDateString().split(' ')[0]}, `
+  string += `${date.toLocaleDateString()}`
+  return string
 }
 
