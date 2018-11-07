9388517e5c00bd8e511c175010f2b8d9d73059bd
micaelsbno <micaelsbno@gmail.com>
2018-11-07 18:15:35 +1100

    rename / refactor ajax methods
    rename methods
    
    refactor ajax methods

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -16,11 +16,11 @@ postMemory = (content, locator, token) => {
     method: "POST",
   })
   .then(res => res.json())
-  .then(res => {console.log(res); appendNewPost(res)})
+  .then(res => {console.log(res); insertNewPostOnTop(res)})
   .catch(function(res){ console.log(res) })
 }
 
-appendNewPost = (response) => {
+insertNewPostOnTop = (response) => {
   const posts = document.querySelector('.posts')
   const postDiv = document.createElement('div')
   postDiv.className = 'post'
@@ -38,10 +38,21 @@ appendNewPost = (response) => {
   posts.insertBefore(postDiv, posts.firstChild)
 }
 
-fetchDashboardPosts = (e, user_id) => {
+formatDate = (dateString) => {
+  date = new Date(dateString)
+  string = ``
+  string += Math.abs(date.getHours() - 12)
+  string += `:${date.getMinutes().toString().length === 0 ? '0' + date.getMinutes() : date.getMinutes()} `
+  string += `${date.getHours() < 12 ? 'am' : 'pm'} `
+  string += `– ${date.toDateString().split(' ')[0]}, `
+  string += `${date.toLocaleDateString()}`
+  return string
+}
+
+fetchMoreUserPosts = (e, user_id) => {
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
     window.removeEventListener('scroll', fetchMoreUserPosts)
-    fetch(`/dashboard_posts/${user_id}?offset=${offset}`)
+    fetch(`/user_posts/${user_id}?offset=${offset}`)
       .then(function(response) {
         return response.json()
       })
@@ -53,13 +64,22 @@ fetchDashboardPosts = (e, user_id) => {
   }
 }
 
-formatDate = (dateString) => {
-  date = new Date(dateString)
-  string = ``
-  string += Math.abs(date.getHours() - 12)
-  string += `:${date.getMinutes().toString().length === 0 ? '0' + date.getMinutes() : date.getMinutes()} `
-  string += `${date.getHours() < 12 ? 'am' : 'pm'} `
-  string += `– ${date.toDateString().split(' ')[0]}, `
-  string += `${date.toLocaleDateString()}`
-  return string
+appendMorePosts = new_posts => {
+  const posts = document.querySelector('.posts')
+  new_posts.forEach(response => {
+    const postDiv = document.createElement('div')
+    postDiv.className = 'post'
+    postDiv.innerHTML = `
+      <p class="post__content">${response.content}</p>
+        <div class="post__weather">
+            <span class="post__weather--icon"><i class="fas fa-sun"></i></span><p class="post__weather--text">${response.weather_description}</p>
+        </div>
+        <div class="post__footer">
+          <p class="post__footer--time">${formatDate(response.created_at)}</p>
+          <p class="post__footer--city">${response.city}</p>
+        </div>
+      </div>
+    `
+    posts.appendChild(postDiv)  
+  })
 }
