3776888733d7289fa31b6cf51b4bfd72b0463bc5
micaelsbno <micaelsbno@gmail.com>
2018-11-07 13:38:04 +1100

    delete unused controller

--- app/controllers/api/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::SessionsController < ApplicationController
-  
-  include SessionsHelper
-
-  def save_location_by_gps
-    session[:gps_location] = {lat: params[:lat], lon: params[:lon]}
-    render json: session[:gps_location]
-  end
-end
