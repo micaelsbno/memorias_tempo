dbd2f816934f1cb3813ace42450908813f69df4e
micaelsbno <micaelsbno@gmail.com>
2018-11-07 16:55:50 +1100

    add test for visiting another user

---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Visit Another user', :type => :feature do
+
+  let (:user) { User.new(username: 'test_user', password: '1234') }
+  before { 
+    user.save
+    10.times { Post.create(content: 'post_content', user_id: user.id) } 
+  }
+
+  it 'shows user posts when visiting url' do
+    visit "/users/#{user.username}"
+
+    expect(page).to have_content 'post_content'
+  end
+end
