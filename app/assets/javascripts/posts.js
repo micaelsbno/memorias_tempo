23f44e5473418697582da6f8fb2992dd859c07c8
micaelsbno <micaelsbno@gmail.com>
2018-11-07 19:53:33 +1100

    fix appending new posts div postioning

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -26,10 +26,8 @@ insertNewPostOnTop = (response) => {
   postDiv.className = 'post'
   postDiv.innerHTML = `
     <p class="post__content">${response.content}</p>
-      <div class="post__weather">
-          <span class="post__weather--icon"><i class="fas fa-sun"></i></span><p class="post__weather--text">${response.weather_description}</p>
-      </div>
       <div class="post__footer">
+        <p class="post__weather--text">${response.weather_description}</p>
         <p class="post__footer--time">${formatDate(convertHours(response.created_at, new Date().getTimezoneOffset()))}</p>
         <p class="post__footer--city">${response.city}</p>
       </div>
@@ -71,10 +69,8 @@ appendMorePosts = new_posts => {
     postDiv.className = 'post'
     postDiv.innerHTML = `
       <p class="post__content">${response.content}</p>
-        <div class="post__weather">
-            <span class="post__weather--icon"><i class="fas fa-sun"></i></span><p class="post__weather--text">${response.weather_description}</p>
-        </div>
         <div class="post__footer">
+          <p class="post__weather--text">${response.weather_description}</p>
           <p class="post__footer--time">${formatDate(response.created_at)}</p>
           <p class="post__footer--city">${response.city}</p>
         </div>
